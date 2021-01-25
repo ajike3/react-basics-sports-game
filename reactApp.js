9268a1a3e7dc24f1d08501e99c835baaf9e9a9bf
ajike3 <69168968+ajike3@users.noreply.github.com>
2021-01-24 22:00:13 -0500

    update normal

--- reactApp.js
+++ reactApp.js
@@ -7,24 +7,42 @@ class Team extends React.Component {
             score: 0
 
         }
-
+        this.shotSound = new Audio('./assets/audio/')
+        this.scoreSound = new Audio('./assets/audio/')
     }
 
     shotHandler = () => {
         let score = this.state.score
-
+        this.shotSound.play()
+        
         if (Math.random()> 0,5){
             score += 1
-        }
+           setTimeout(() =>{            
+            this.scoreSound.play()
+        }, 100)
+
+    }    
         
         this.setState((state, props) =>({
-        shots: state.shots + 1
+        shots: state.shots + 1,
+        score
         }))
     }
         render (){
-        return(
-            <div className="Team">
-                <h2>{this.props.name}</h2>
+            let shotPercentageDiv
+            
+            if (this.state.shots) {}  
+            const shotPercentage = this.state.score /this.state.shots * 100   
+            shotPercentageDiv = (
+                <div>
+                    <strong> Shooting %: {shotPercentage}</strong>
+                </div>
+            )
+        }
+
+            return(
+                <div className="Team">
+                    <h2>{this.props.name}</h2>
 
                 <div className="identity">
                     <img src= {this.props.logo} alt={this.props.name}/>
@@ -37,28 +55,42 @@ class Team extends React.Component {
                 <div>
                     <strong>Score:</strong> {this.state.score}
                 </div>
-            
+                <div>
+                    shotPercentage
+                </div>    
 
+                <div>
+                    <strong>Shooting %: {shotPercentage}</strong> {this.state.score}</strong>
+                </div>    
             <button> oneClick={this.shotHandler}>Shoot!</button>
             </div>
         )
     }       
 }
 
+
+function
+
 function App(props) {
     return (
         <div className ="App">
                 <h1>Welcome to the sports game starter.</h1>
-            <div className: "stats">
+            <div className="stats">
             <Team 
-                name= "Raiders" 
+                name="Raiders" 
                 logo="./assets/images/raiders"  
-            />      
+            />     
+
+            <div className="versus">
+                <h1>VS</h1>
+            </div>
+
             <Team
                 name="Lakers"
                 logo="./assets/images/lakers"
             />    
-        </div>
+         </div>
+        </div>    
     )
 }
  
